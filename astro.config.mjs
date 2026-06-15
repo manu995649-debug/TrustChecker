705cf66c230cfaf79a3468afa659eb93bc665a3a
manu995649-debug <manu995649@gmail.com>
2026-06-15 18:31:39 +0530

    Configure @astrojs/sitemap integration and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://trustchecker.com',
+  integrations: [sitemap()],
   adapter: vercel(),
 });
